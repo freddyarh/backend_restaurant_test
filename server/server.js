e09d8e1424a66db39339b532f4eceb48792dc190
Fredy <freddyarh13@gmail.com>
2021-06-08 07:37:46 -0500

    Se installo CORS, para la conexion del fronted y bachend

--- server/server.js
+++ server/server.js
@@ -4,25 +4,27 @@ const mongoose = require('mongoose');
 
 const Producto = require('./models/producto');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 let app = express();
+app.use(cors());
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
- 
+
 // parse application/json
 app.use(bodyParser.json())
 
 // Configuración global de rutas
 app.use(require('./routes/index'));
 
-mongoose.connect('mongodb://localhost:27017/restaurante',{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}, (err, res) => {
-    if(err) throw err;
+mongoose.connect('mongodb://localhost:27017/restaurante', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err, res) => {
+    if (err) throw err;
 
     console.log('Base de datos online');
 });
 
-   
+
 app.listen(3000, () => {
     console.log('Escuchando puerto 3000');
 })
